a6df6515c8added1f61600438b7d4fe2127d8ca7
FariaaFaheem <fariaa.faheem@gmail.com>
2022-08-11 09:58:17 +0500

    Controller updated

--- controller.v
+++ controller.v
@@ -6,6 +6,7 @@ module controller(
 parameter IMG_SIZE = 256;
 parameter KER_SIZE = 3;
 
+reg done;
 reg [7:0] count, temp;
 
 always@(posedge clk) 
@@ -17,23 +18,29 @@ begin
         filtAddr <= 0;
         count <= 0;
         temp <= 0;
+        done <= 0;
     end
     else
     begin
-        imAddr <= imAddr + 1;
-        temp <= 1;
-		/*as the imAddr will be updated we need to subtract 1, and in order to jump to new column
-		  count variable is used */
-		if (temp == (IMG_SIZE-2))
+        if (done == 0)
         begin
-            #1000;//just break out of the block
+            imAddr <= imAddr + 1;
+            temp <= temp + 1;
+            /*as the imAddr will be updated we need to subtract 1, and in order to jump to new column
+              count variable is used */
+            if (temp == ((IMG_SIZE-2)*(IMG_SIZE-2)))
+            begin
+                done <= 1;
+                imAddr <= 0;
+                kAddr <= 0;
+                filtAddr <= 0;
+            end
+            else if (imAddr == (IMG_SIZE - KER_SIZE + count - 1))
+            begin
+                count <= count + IMG_SIZE;
+                imAddr <= imAddr + IMG_SIZE - KER_SIZE;
+            end
         end
-        else if (imAddr == (IMG_SIZE - KER_SIZE + count - 1))
-        begin
-            count <= count + IMG_SIZE;
-            imAddr <= imAddr + IMG_SIZE - KER_SIZE;
-        end
-        
     end
 end
 
